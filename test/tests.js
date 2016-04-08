942f004f59f8895eb20cd3cfc89edf811cac864d
alu0100505009 <alu0100505009@ull.edu.es>
2016-04-08 09:49:13 +0000

    Prueba primer test

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,12 @@ var expect = chai.expect;
 /* ******PRUEBAS PARA LA CLASE MEDIDA****** */
 describe("CSV", function() 
 {
-        
+     describe('#calculate', function() {
+        it ('deberia saber el numero de lineas',function() {
+          this.original = "\"color\",\"numero\",\"objeto\"\n\"azul\",\"2\",\"coche\"\n\"rojo\",\"6,2\",\"lápiz\"";
+          expect(calculate(this.original).length).to.deep.equal(3);
+        });
+     });    
     
         
 });
